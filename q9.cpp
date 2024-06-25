4edd1b0aecce3610b059ccf9e2833b5f79e1f909
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:32:45 +0530

    Update q9.cpp

--- q9.cpp
+++ q9.cpp
@@ -1,42 +1,90 @@
 #include<iostream>
 #include<string>
+#include<vector>
 #include<algorithm>
 #include<cmath>
 #include<numeric>
-#include<set>
-#include<vector>
 #include<unordered_map>
 using namespace std;
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int k;
-        cin>>k;
-        string s;
-        cin>>s;
-        
-        unordered_map< char, int > m;
-        m.clear();
-        int n=s.size();
-        int i=0,j=0;
-        int count=0;
-        m[s[0]]=1;
-        while(i<n && j<n){
-            
-            if(m.size()<k){
-                j++;m[s[j]]++;
-            }
-            else if(m.size()==k){
-                count++;
-                j++;m[s[j]]++;
-            }
-            else{
-                if(m[s[i]]==1){m.erase(s[i]);i++;}
-                else{m[s[i]]--;i++;}
+        int n,m;
+        cin>>n>>m;
+        int a[n][m];
+        int b[n][m];
+        int b_clone[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cin>>a[i][j];
             }
         }
-        cout<<count<<endl;
-    }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cin>>b[i][j];
+                b_clone[i][j]=b[i][j];
+            }
+        }
+        if(n==1 || m==1){cout<<"YES"<<endl;continue;}
+        for(int i=0;i<n;i++){
+            sort(b_clone[i],b_clone[i]+m);
+        }
+        int l=-1;
+        for(int i=0;i<n;i++){
+            int curr_row_a[m];
+            int curr_row_b[m];
+            for(int j=0;j<m;j++){
+                curr_row_a[j]=a[i][j];
+            }
+            sort(curr_row_a,curr_row_a+m);
+            l=-1;
+            for(int j=0;j<n;j++){
+                if(curr_row_a[0]==b_clone[j][0]){l=j;break;}
+            }
+            if(l==-1){break;}
+            else{
+                for(int j=0;j<m;j++){
+                    if(curr_row_a[j]!=b_clone[l][j]){l=-1;break;}
+                }
+                if(l==-1){break;}
+            }
+            
 
+        }
+        if(l==-1){cout<<"NO"<<endl;}
+        else{
+            int b_clawn[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    b_clawn[i][j]=b[j][i];
+                }
+            }
+            for(int i=0;i<m;i++){
+                sort(b_clawn[i], b_clawn[i]+n);
+            }
+            for(int i=0;i<m;i++){
+                int curr_column_a[n];
+                for(int j=0;j<n;j++){
+                    curr_column_a[j]=a[j][i];
+                }
+                sort(curr_column_a,curr_column_a+n);
+                l=-1;
+                for(int j=0;j<m;j++){
+                    if(curr_column_a[0]==b_clawn[j][0]){l=j;break;}
+                }
+                if(l==-1){break;}
+                else{
+                    for(int j=0;j<n;j++){
+                        if(curr_column_a[j]!=b_clawn[l][j]){l=-1;break;}
+                    }
+                    if(l==-1){break;}
+                }
+                if(l==-1){break;}
+                
+            }
+            if(l==-1){cout<<"NO"<<endl;}
+            else{cout<<"YES"<<endl;}
+        }
+    }
 }
