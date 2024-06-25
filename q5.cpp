4b981ced200bd92bcd0e7c544179e4de515ede7d
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:29:23 +0530

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -5,33 +5,48 @@
 #include<numeric>
 #include<set>
 #include<vector>
-#include<map>
 #include<unordered_map>
+#include<map>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n,k;
-        cin>>n>>k;
-        unordered_map<int, int> m;
-        m.clear();
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-            m[a[i]]=i+1;
-        }
-        sort(a,a+n);
-        int i=0,j=n-1;
-        while(i<j){
-            int sum=a[i]+a[j];
-            if(sum==k){break;}
-            else if(sum>k){j--;}
-            else{i++;}
-        }
-        if(i==j){cout<<-1<<" "<<-1<<endl;}
-        else{cout<<m[a[i]]<<" "<<m[a[j]]<<endl;}
-    }
 
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    int u[n];
+    map<int, vector< int > > mymap;
+    for(int i=0;i<n;i++){
+        cin>>u[i];
+        mymap[u[i]].push_back(i);
+    }
+    int a[k];
+    int b[k];
+    for(int i=0;i<k;i++){
+        cin>>a[i]>>b[i];
+        int a_s=mymap[a[i]].size();
+        int b_s=mymap[b[i]].size();
+        if(a_s==0 || b_s==0){cout<<"NO"<<endl;}
+        else{
+            
+            if(mymap[a[i]][0]>mymap[b[i]][b_s-1]){cout<<"NO"<<endl;}
+            else{cout<<"YES"<<endl;}
+        }
+    }
     
+    return;
 }
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin >> t;
+    // long long int a=1999999998;
+    // long long int b=1000000000;
+    // cout<<"a: "<<a<<endl;
+    // cout<<"b: "<<b<<endl;
+    // cout<<"a+b: "<<a+b<<endl;
+    while(t--){
+        solve();
+    }
+}
+
