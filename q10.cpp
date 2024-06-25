fac7c6902a52d0ef6835befcd84b8c91a3564b10
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:33:23 +0530

    Update q10.cpp

--- q10.cpp
+++ q10.cpp
@@ -5,29 +5,51 @@
 #include<numeric>
 #include<set>
 #include<vector>
+#include<unordered_map>
+#include<map>
 using namespace std;
+
+void solve(){
+    long long int n;
+    cin>>n;
+    int a[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    long long int count =0;
+    // map<int, long long int> mymap;
+    // for(int i=0;i<n;i++){
+    //     if(a[i]!=2){count = count + mymap[a[i]];mymap[a[i]]++;}
+    //     else{count = count + mymap[1];mymap[1]++;}
+        
+    // }
+	// cout << count <<endl;
+    map<long long int, long long int> mymap;
+    for(int i=0;i<n;i++){
+        if(a[i]!=2){mymap[a[i]]++;}
+        else{mymap[1]++;}
+    }
+    for(auto& elem : mymap){
+        count = count + (elem.second*(elem.second - 1))/2;
+    }
+    cout<<count<<endl;
+    
+  // 
+    
+    
+    return;
+}
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int t;
     cin>>t;
+    
     while(t--){
-        int n,k;
-        cin>>n>>k;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-
-        for(int i=0;i<=n-k;i++){
-            int max_s=a[i];
-            for(int j=i;j<i+k;j++){
-                max_s=max(max_s, a[j]);
-            }
-            cout<<max_s<<" ";
-        }
-        cout<<endl;
+        solve();
     }
-
-
-
-
 }
+
