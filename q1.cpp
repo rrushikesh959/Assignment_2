d238dd2e8fa78e428d747cf9b1d94bad6a3ddfa5
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:26:18 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -5,29 +5,35 @@
 #include<numeric>
 #include<set>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
-        int n,k;
-        cin>>n>>k;
+        int n;
+        cin>>n;
         int a[n];
         for(int i=0;i<n;i++){
             cin>>a[i];
         }
-
-        int i=0,j=n-1;
-        int sum = accumulate(a,a+n,0);
-        int min_length=-1;
-        
-        while(i<=j){
-            if(sum>=k){min_length=j-i+1;}
-            if(a[i]<=a[j]){sum=sum-a[i];i++;}
-            else{sum=sum-a[j];j--;}
-
+        if(n%2==1){cout<<"YES"<<endl;}
+        else{
+            int b[n-1];
+            for(int i=0;i<n-1;i++){
+                b[i]=a[i+1]-a[i];
+            }
+            long long int sum=0;
+            for(int i=0;i<n-1;i=i+2){
+                sum=sum+b[i];
+            }
+            if(sum>=0){cout<<"YES"<<endl;}
+            else{cout<<"NO"<<endl;}
         }
-        cout<<min_length<<endl;
     }
 }
+
