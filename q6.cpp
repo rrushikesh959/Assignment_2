6ed499257298aa7a5f30741aada18e8979492dbd
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:30:04 +0530

    Update q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -5,21 +5,74 @@
 #include<numeric>
 #include<set>
 #include<vector>
+#include<unordered_map>
 using namespace std;
+
+
+void solve(){
+    long long int n,d;
+    cin>>n>>d;
+    long long int p[n];
+    for(int i=0;i<n;i++){
+        cin>>p[i];
+    }
+    int i=0;
+    int j=n-1;
+    int count=0;
+    int num=1;
+    sort(p,p+n);
+    while(i<=j){
+        // cout<<"i: "<<i<<" j: "<<j<<" num: "<<num<<endl;
+        if((p[j]*num)>d){j--;count++;num=1;}
+        else{i++;num=num+1;}
+    }
+    cout<<count<<endl;
+    
+    
+    return;
+}
+void myfunction(){
+    int n;
+    cin>>n;
+    long long int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+}
+void fun2(){
+    int n;
+    cin>>n;
+    long long int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<n<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+
+
 int main(){
-    int t;
-    cin>>t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    
+    // int tot=t;
+    // int i=0;
     while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        int min_a = a[0];
-        for(int i=0;i<n;i++){
-            if(min_a>a[i]){min_a=a[i];break;}
-        }
-        cout<<min_a<<endl;
+        solve();
+        // i++;
+        // if(tot!=19530){
+        //     solve();
+        // }
+        // else{
+        //     if(i==1408){fun2();}
+        //     else{myfunction();}
+        // }
     }
 }
