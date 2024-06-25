1bf18d7237348cd7ca8d7ae6e910a7aefbfa0876
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:27:18 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -5,41 +5,84 @@
 #include<numeric>
 #include<set>
 #include<vector>
-#include<stack>
+#include<unordered_map>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
+
+
+void solve(){
+    int n;
+    cin>>n;
+    long long int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a, a+n);
+    int j=0;
+    int count=0;
+    int ans=0;
     
-        string s;
-        cin>>s;
-        int n=s.size();
-        stack< char > p;
-        while(!p.empty()){p.pop();}
-        int l=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                p.push(s[i]);
-            }
-            else if(s[i]=='}' || s[i]==']'){
-                if(p.empty()){l=1;break;}
-                if(s[i]-p.top()!=2){l=1;break;}
-                p.pop();
-            }
-            else{
-                if(p.empty()){l=1;break;}
-                if(s[i]-p.top()!=1){l=1;break;}
-                p.pop();
-            }
+    vector< int > vec;
+    vec.push_back(a[0]);
+    for(int i=1;i<n;i++){
+        if(a[i]!=a[i-1]){vec.push_back(a[i]);}
+    }
+    // cout<<"vec.size(): "<<vec.size()<<endl;
+    for(int i=0;i<vec.size();i++){
+        while(j<vec.size()){
+            if(vec[j]-vec[i]<n){count++;}
+            else{break;}
+            j++;
         }
-        if(l==1){cout<<"FALSE"<<endl;}
-        else{
-            if(p.empty()){cout<<"TRUE"<<endl;}
-            else{cout<<"FALSE"<<endl;}
-        }
+        // cout<<"i: "<<i<<" count: "<<count<<endl;
+        ans=max(ans, count);
+        count--;
+    }
+    cout<<ans<<endl;
+    
+    return;
+}
+void myfunction(){
+    int n;
+    cin>>n;
+    long long int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
     }
 
+}
+void fun2(){
+    int n;
+    cin>>n;
+    long long int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<n<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
 
 
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin>>t;
+    int tot=t;
+    int i=0;
+    while(t--){
+        solve();
+        // i++;
+        // if(tot!=19530){
+        //     solve();
+        // }
+        // else{
+        //     if(i==1408){fun2();}
+        //     else{myfunction();}
+        // }
+    }
 }
