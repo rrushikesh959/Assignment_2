36d627d8997d428245ab3d2c75d0fcc23dea36c3
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:32:03 +0530

    Update q8.cpp

--- q8.cpp
+++ q8.cpp
@@ -1,35 +1,75 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
-#include<cmath>
-#include<numeric>
-#include<set>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n,k;
-        cin>>n>>k;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            int temp=0;
-            for(int j=i;j<n;j++){
-                temp= temp^a[j];
-                if(temp==k){count++;}
-                
+#define ll long long
+#define pb push_back
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define ff first
+#define ss second
+
+#define testcase
+void solve(){
+ 	int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int len = 1;len <= n / 2;++len) {
+        if (n%len == 0) {
+
+
+            bool yes = 1;
+            int ok = 0;
+            for (int i = 0; i < len && yes;i++) {
+                vector<int> cnt(26,0);
+                cnt[s[i] - 'a']++;
+                for (int j = 1; j < n / len;++j) {
+                    cnt[s[len * j + i] - 'a']++;
+                }
+                int cnt1 = 0,p1 = 0,p2 = 0;
+                for (int i = 0;i<26;++i) {
+                    if (cnt[i] > 0) {
+                        ++cnt1;
+                        if (cnt1 == 1) {
+                            p1 = cnt[i];
+                        }
+                        if (cnt1 == 2) p2 = cnt[i];
+                    }
+
+                }
+                if (cnt1 > 2 or (p1 > 1 && p2 > 1)) {
+                    yes = 0;
+                    break;
+                }
+                if (p2 > 0) ok++;
+                if (ok > 1) {
+                    yes = 0;
+                    break;
+                }
             }
+            if (yes) {
+                cout << len;
+                return;
+            }
+
         }
-        cout<<count<<endl;
     }
 
+    cout << n;
 }
 
 
 
-
+signed main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int tt = 1;
+#ifdef testcase
+      cin >>tt;
+#endif
+  while (tt--) {
+    solve();
+    cout << "\n";
+  }
+  return 0;
+}
