f34ffadc10abbeb8dc5d910fabdea36dc32012ab
rrushikesh959 <139304957+rrushikesh959@users.noreply.github.com>
2024-06-25 23:28:40 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -5,28 +5,41 @@
 #include<numeric>
 #include<set>
 #include<vector>
+#include<unordered_map>
 using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    long long int a[2*n];
+    for(int i=0;i<2*n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+2*n);
+    long long int sum=0;
+    for(int i=1;i<n;i++){
+        sum=sum+abs(a[i]-a[i-1])+abs(a[n+i]-a[n+i-1]);
+    }
+    cout<<sum<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" "<<a[n+i]<<endl;
+    }
+
+
+}
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int t;
-    cin>>t;
+    cin >> t;
+    // long long int a=1999999998;
+    // long long int b=1000000000;
+    // cout<<"a: "<<a<<endl;
+    // cout<<"b: "<<b<<endl;
+    // cout<<"a+b: "<<a+b<<endl;
     while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        int i=0;
-        int j=n-1;
-        int sum = accumulate(a,a+n,0);
-        int max_sum=sum;
-        
-        while(i<=j){
-            if(sum>max_sum){max_sum=sum;}
-            if(a[i]<=a[j]){sum=sum-a[i];i++;}
-            else{sum=sum-a[j];j--;}
-        }
-        cout<<max_sum<<endl;
-        
+        solve();
     }
 }
+
